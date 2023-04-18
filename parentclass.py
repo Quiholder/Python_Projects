78e99501c62ca8387fedd3114cf5f51b4e74e122
Quiholder <119394603+Quiholder@users.noreply.github.com>
2023-04-17 22:45:41 -0400

    parent/child class and attributes

---
+++ parentclass.py
@@ -0,0 +1,19 @@
+
+#parent class
+class user:
+    name= 'no name provided'
+    email=' '
+    password= '9033'
+    account number=78
+
+#child class and attributes
+class employee(user):
+    hours_worked=40
+    date_hired='general'
+
+#child class and attributes 
+class customer(user):
+    phone_number=' '
+    email_address=True
+    
+    
